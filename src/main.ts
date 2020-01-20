b9c1f524045732b4737780ae483da2a45dbc27f3
Lucas OMS <lucas.oms@etu.unice.fr>
2020-01-20 17:22:38 +0100

    Play, récupération des objets connectés, confirmation, lancement de la partie

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {Logger} from '@nestjs/common';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useLogger(app.get(Logger));
+    app.enableCors();
     await app.listen(4444);
 }
 
